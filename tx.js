3669865297c1ea929a95458e4f21cb48df770eaa
Stanislas Marion <stanislas.marion@gmail.com>
2014-09-15 16:01:37 +0200

    better opcodes

--- tx.js
+++ tx.js
@@ -2,13 +2,7 @@ var buffertools = require('buffertools')
 buffertools.extend()
 var EC = require('elliptic').ec
 var hash = require('hash.js')
-
-var OPCODES = {
-  OP_DUP: new Buffer('76', 'hex'),
-  OP_HASH160: new Buffer('a9', 'hex'),
-  OP_EQUALVERIFY: new Buffer('88', 'hex'),
-  OP_CHECKSIG: new Buffer('ac', 'hex')
-}
+var opcodes = require('./constants').opcodes
 
 var hexTransaction = new Buffer('010000000438609f5db075726205cdaebccbd82a7a63fa7b48fe119c33d3d9944984f7a7f2010000006b483045022100c433099a511e6acc332a5963d22cde4afc6a945d951e11de5f617056cf22a2d0022075218dffaa31902fe6f2e6ee754106245f872c962d13836d040a7db97c7923ab01210294ba2c89e4554a0b7324a654a62843d7bf8e67140c4dd192d0f999abab568816ffffffff4a45a85c9d31b94e35be88c4e18cce42a3c6ddb6b10bdce0858edc17dc688ee1000000006b483045022100fab2303e02394e620df8974c15e64ae338081ec9903ae90ecebfc2c4c679c9cc0220009054f8cd6add400c341d268e1412fde56f732632fb41eb368d313ed406b4ce012103db0fc4243afb5dcb6b164f30661cdf29dbb637017e59083e5b91a4e85f86555bffffffff45f4ee45fa787248a8d26dffd1b19488a4c6e88e70db466c896912d9c0957ac2000000006a4730440220398ee1a6fa9f5b7970feba02aa0d3435a740ff76dfbed7f24d452e0c48328da80220394a4b64383e2f75fcd19ade41290dadf2cbdb34eaf1b03032864cc5fd86ff81012103db0fc4243afb5dcb6b164f30661cdf29dbb637017e59083e5b91a4e85f86555bffffffff2d3efde69d53ec18950d1107f9244b10e6cebcd57b8c0b7d9309dc300c6a42b4010000006a47304402201361b054e039b1218ad1b8d6a8b89bd1d31ced3a8cf45b94dc5ea7c8b1166d9a022060d9552fde6a37eb35ac30793cb1625decff314e194a61821bcd7b51ce6071e60121030cb9d78bb707d0deae4b6497c7cdb4154d50a4053b982219baab64b53644517fffffffff0293d90f00000000001976a9144b87255bc72f200a4a32c826e286d3ceb2fb5c2988ac40420f00000000001976a9148488eee34d08d2fef170566301f1befbdda8b6c088ac00000000', 'hex')
 var prevHash = dsha256(hexTransaction)
@@ -44,12 +38,12 @@ tx.outs[0].value = new Buffer(8)
 tx.outs[0].value.clear()
 writeUInt64LE(tx.outs[0].value, amount, 0)
 tx.outs[0].scriptPubKey = buffertools.concat(
-  OPCODES.OP_DUP,
-  OPCODES.OP_HASH160,
+  new Buffer([opcodes.OP_DUP]),
+  new Buffer([opcodes.OP_HASH160]),
   new Buffer('14', 'hex'),
   hash160,
-  OPCODES.OP_EQUALVERIFY,
-  OPCODES.OP_CHECKSIG
+  new Buffer([opcodes.OP_EQUALVERIFY]),
+  new Buffer([opcodes.OP_CHECKSIG])
 )
 tx.outs[0].scriptPubKeySize = numToVarInt(tx.outs[0].scriptPubKey.length)
 
